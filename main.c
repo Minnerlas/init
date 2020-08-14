d94c280927f873feb2bef5cb16f08645902c358e
Minnerlas <nikilirikili@gmail.com>
2020-08-14 21:17:08 +0200

    A

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 int main() {
 	printf("Hello\n");
